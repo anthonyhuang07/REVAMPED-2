bbe15aea55b41503bfe77cae1f05960b3d66d3b1
Anthony Huang <anthonyhuang07@icloud.com>
2024-06-05 22:17:06 -0400

    scrapped idea

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,10 @@
-// Select the Earth element
 const earth = document.getElementById('earth');
-const space = document.querySelector('space');
+const titleContent = document.getElementById('title-content');
 
-// Function to update the position of the Earth based on scroll progress
 function updateEarthPosition() {
-    // Get the scroll position
-    const scrollDistance = window.scrollY;
 
-    // Define the maximum scroll distance for the animation
-    const maxScrollDistance = 200; // Adjust as needed
+    const scrollDistance = window.scrollY;
+    const maxScrollDistance = 200;
 
     // Limit the scroll distance to the maximum scroll distance
     const limitedScrollDistance = Math.min(scrollDistance, maxScrollDistance);
@@ -17,17 +13,9 @@ function updateEarthPosition() {
     const scrollPercentage = limitedScrollDistance / maxScrollDistance;
 
     // Calculate the new translation values based on scroll progress
-    const translateX = 50 - 25 * (scrollPercentage);
-    const translateY = 87.5 - 5 * (scrollPercentage);
+    const scale = 1 - 0.5 * (scrollPercentage);
 
-    // Apply the new translation values to the Earth element
-    earth.style.transform = `translate(${translateX}%, ${translateY}%)`;
-
-    if (scrollDistance >= 200) {
-        earth.style.position = 'absolute';
-    } else {
-        earth.style.position = 'fixed';
-    }
+    titleContent.style.transform = `scale(${scale})`;
 }
 
 // Add event listener to listen for scroll events
